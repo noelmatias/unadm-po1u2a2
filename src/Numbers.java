6ca747e64301f1d5380cc8c1a9e5387e85ecd455
Noel Matías <n@tid.do>
2021-05-03 23:23:33 -0500

    Menu

---
+++ src/Numbers.java
@@ -0,0 +1,45 @@
+// ISO-8859-1 encoding
+import java.util.Scanner;
+
+public class Numbers {
+    static String[] menu = {
+            "1.- Registrar número:",
+            "2.- Es capicúa:",
+            "3.- Es primo:",
+            "4.- Elevado al cuadrado su producto es:",
+            "5.- Salir."
+    };
+
+    static void ClearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public static void main(String[] args) {
+        // Set class for user input
+        Scanner userInput = new Scanner(System.in);
+        boolean running = true;
+        while (running){
+            ClearScreen();
+
+            // Print menu
+            byte i;
+            for (i = 0; i < menu.length; i++){
+                System.out.println(menu[i]);
+            }
+
+            byte menuItem = userInput.nextByte();
+            switch (menuItem) {
+                case 1 -> System.out.println(menu[0]);
+                case 2 -> System.out.println(menu[1]);
+                case 3 -> System.out.println(menu[2]);
+                case 4 -> System.out.println(menu[3]);
+                case 5 -> {
+                    running = false;
+                    ClearScreen();
+                }
+                default -> System.out.println("Entrada no válida");
+            }
+        }
+    }
+}
